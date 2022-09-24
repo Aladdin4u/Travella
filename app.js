8eeec3d4ffaccd31fcdeea0a3aa378139a0987f5
aladdin4u <taofikakinkumi@gmail.com>
2022-09-24 17:06:37 +0100

    modify index file

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const app = express()
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const homeRoutes = require('./routes/home')
+
+// LOad
 dotenv.config({ path: './config/.env'})
 
 connectDB()
 
-const PORT = process.env.NODE_ENV || 2022
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.listen(
-    PORT, () => {
-        console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-    }
-)
+app.use('/', homeRoutes)
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})  
