7cfeeb1a388aa8eae54df3e7692143ef94202d25
aladdin4u <taofikakinkumi@gmail.com>
2023-04-05 21:14:50 +0100

    integretion of stripe

--- client/src/pages/stripe/Stripe.jsx
+++ client/src/pages/stripe/Stripe.jsx
@@ -1,31 +1,32 @@
-
 import axios from "axios";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { PaymentElement } from "@stripe/react-stripe-js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const Reverse = ({ setOpen, hotelId }) => {
+const Stripe = () => {
+  const [options, setOptions] = useState([
+    {
+      clientSecret: client_secret,
+      // Fully customizable with appearance API.
+      appearance: {
+        /*...*/
+      },
+    },
+  ]);
   const stripePromise = loadStripe(
     "pk_test_51MsyNdB3KrsTgH4dxD0TcUPW7aRI3LiqBzhHsUn6FP006wMVcWDToanngoRLizmynPDxpxbW9ad2V7NdaSlERIAY00VzSVUQ6O"
   );
-useEffect (() => {
-    loadStripePage()
-},[])
+  useEffect(() => {
+    loadStripePage();
+  }, []);
   const loadStripePage = async () => {
     setLoading(true);
     try {
       const client_secret = axios.post(
         `http://localhost:8000/api/checkout/create-checkout-session`
       );
-      const options = {
-        // passing the client secret obtained in step 3
-        clientSecret: client_secret,
-        // Fully customizable with appearance API.
-        appearance: {
-          /*...*/
-        },
-      };
+      setOptions((prev) => ({ ...prev, clientSecret: client_secret }));
     } catch (error) {}
   };
   return (
@@ -40,4 +41,4 @@ useEffect (() => {
   );
 };
 
-export default Reverse;
+export default Stripe;
